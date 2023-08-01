0b9f0beb8e8a0eac7bb6309631a0a38062031ebf
rfks <15876610+rfks@users.noreply.github.com>
2023-08-01 20:22:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    var skills = document.getElementById("skills").value;
+    var skillsMember = document.getElementById("skillsMember").value;
+    var skillsMember = skills + skillsMember;
+    document.getElementById("skillsMember").value = skillsMember;
+}
